8dd7b2b83ecbe921d42e7ff1ed46a5eba892e556
lumhardesign <emrebaglayici1@gmail.com>
2022-07-18 16:43:23 +0300

    Mini project has finished.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
 
-function App() {
+const App = () => {
+  const [hotels, setHotels] = useState(null)
+
+  const fetchData = async () => {
+    const response = await fetch('/.netlify/functions/getHotels')
+    const responseBody = await response.json()
+    setHotels(responseBody.data.hotel_data.values)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  console.log(hotels)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <h1>My Hotels</h1>
+        {hotels?.map(hotel => (
+            <div className="hotel">
+              <h2>{hotel.name}</h2>
+              <p>{hotel.rating}</p>
+            </div>
+        ))}
+      </div>
+  )
 }
 
-export default App;
+export default App
